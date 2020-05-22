6105bd32b6ba848b9006095637328ab017633528
Marissa-L <mlook@uw.edu>
2020-05-21 23:18:55 -0700

    Completed Summary Information script, added Report Render and Index files

---
+++ scripts/summary information script.R
@@ -0,0 +1,15 @@
+# Summary Information Script
+movies <- read.csv("data/movies.csv", stringsAsFactors = FALSE)
+
+get_summary_info <- function(dataset) {
+  dataset_info <- list()
+  dataset_info$number_of_cols <- length(dataset)
+  dataset_info$colnames <- colnames(dataset)
+  dataset_info$char_types <- sapply(dataset, class)
+  dataset_info$number_of_rows <- nrow(dataset)
+  dataset_info$example_movies <- sample(unique(dataset$name), size = 10)
+  return(dataset_info)
+}
+
+get_summary_info(movies)
+library(lintr)
