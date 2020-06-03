2ccb0f326128408f696c5fb3d37d0714da43e1c0
Marissa-L <mlook@uw.edu>
2020-06-03 16:43:02 -0700

    overview page

--- app_ui.R
+++ app_ui.R
@@ -2,9 +2,11 @@
 source("audience_rating_page.R")
 source("income_budget_page.R")
 source("rating_budget_page.R")
+source("overview_page.R")
 
 ui <- navbarPage(
   "Movie Data",
+  overview_page,
   audience_rating_page,
   income_budget_page,
   rating_page
---
+++ overview_page.R
@@ -0,0 +1,14 @@
+library(shiny)
+library(knitr)
+
+overview_page <- tabPanel(
+  "Overview",
+  overview_main
+  )
+
+
+overview_main <- fluidPage(
+  h1("Overview"),
+  p("Our project focuses on analyzing specific features of movies and how they have changed over time. By looking at different qualities of movies such as increases in budget, the ratings of movies, and general income of movies, we can get a better understanding of which types of movies have seen a growth in budget and popularity. Our dataset, called",
+    a("movies,", href="https://www.kaggle.com/danielgrijalvas/movies"), "spans from 1986 - 2016. It provides data such as the budget, rating, gross revenue, and genre of the movie. Using these characteristics, sorted by year, we can evaluate the trends and patterns of these movies over time, if any, to get a better understanding of the future of the movie industry."),
+)
